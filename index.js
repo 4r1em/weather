7ad89a00283be69e1a57baa9be598963259c5164
4r1em <art.beloshapka@gmail.com>
2021-08-04 11:16:52 +0300

    added check by city

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const request = require("request");
 const app = new express();
-
+const cites = ["kiev", "moscow", "london", "minsk"]
 
 app.use(express.json())
 app.post('/weather', async (req, res) => {
-    const city = req.body.city;
-    // const day = req.body.day;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8bdbd54d3dcf72500df09e38fc2ed127`
-
-    await request({ url: url, json: true }, (err, { body }) => {
-        res.send(`Today in the city ${city} weather ${body.weather[0].main.toLowerCase()} wind speed ${String(body.wind.speed)}km/h and temperature ${String(Math.floor(body.main.temp - 273))}℃`);
-    });
-
-
-
+    const city = req.body.city
+    if (!city) res.send("Need city")
+    if (!cites.includes(city.toLowerCase())) res.send(`Need city ${cites}`)
+    else {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8bdbd54d3dcf72500df09e38fc2ed127`
+        await request({ url: url, json: true }, (err, { body }) => {
+            res.send(`Today in the city ${city} weather ${body.weather[0].main.toLowerCase()} wind speed ${String(body.wind.speed)}km/h and temperature ${String(Math.floor(body.main.temp - 273))}℃`);
+        });
+    }
 })
-
-
 app.listen(3020, () => {
     console.log("Server work");
 })
