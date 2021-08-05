71bc25c8d46ae69b45843d02e40395e6592af34d
4r1em <art.beloshapka@gmail.com>
2021-08-05 15:34:51 +0300

    Added the ability to search by multiple coordinates

--- index.js
+++ index.js
@@ -5,20 +5,30 @@ const app = new express();
 
 app.use(express.json())
 app.post('/weather', async (req, res) => {
-    const latitude = req.body.latitude
-    const longitude = req.body.longitude
-    if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180 || !latitude || !longitude) res.send("Enter latitude between -90 and 90 and longitude between -180 and 180")
-    else {
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=8bdbd54d3dcf72500df09e38fc2ed127`
-        await request({ url: url, json: true }, (err, { body }) => {
-            if (body.name) {
-                res.send(`Today in the city ${body.name} weather ${body.weather[0].main.toLowerCase()} wind speed ${String(body.wind.speed)}m/h and temperature ${String(Math.floor(body.main.temp - 273))}℃`);
-            } else {
-                res.send(`Today at the coordinates ${latitude}, ${longitude} the weather ${body.weather[0].main.toLowerCase()} wind speed ${String(body.wind.speed)} m/h and temperature ${String(Math.floor(body.main.temp - 273))} ℃ `);
-            }
-        });
+    const coordinates = req.body.coordinates
+    let latitude
+    let longitude
+    let arrEnd = [];
+    for (item of coordinates) {
+        latitude = item[0]
+        longitude = item[1]
+        if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180 || !latitude || !longitude) res.send("Enter latitude between -90 and 90 and longitude between -180 and 180")
+        else {
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=8bdbd54d3dcf72500df09e38fc2ed127`
+            await request({ url: url, json: true }, (err, { body }) => {
+                if (body.name) {
+                    arrEnd.push(`Today in the city ${body.name} weather ${body.weather[0].main.toLowerCase()} wind speed ${String(body.wind.speed)}m/h and temperature ${String(Math.floor(body.main.temp - 273))}℃`);
+                } else {
+                    arrEnd.push(`Today at the coordinates ${latitude}, ${longitude} the weather ${body.weather[0].main.toLowerCase()} wind speed ${String(body.wind.speed)} m/h and temperature ${String(Math.floor(body.main.temp - 273))} ℃ `);
+                }
+                if (coordinates.length == arrEnd.length) res.send(arrEnd.join("\n"))
+            })
+        }
 
     }
+
+
+
     // const city = req.body.city
     // if (!city) res.send("Need city")
     // if (!cites.includes(city.toLowerCase())) res.send(`Need city ${cites}`)
