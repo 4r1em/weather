76033a05ff141f72ab8a671288a0ac06b365c34a
4r1em <art.beloshapka@gmail.com>
2021-08-05 14:48:42 +0300

    error correction

--- index.js
+++ index.js
@@ -7,17 +7,12 @@ app.use(express.json())
 app.post('/weather', async (req, res) => {
     const latitude = req.body.latitude
     const longitude = req.body.longitude
-    let city = ""
-    // const coordinates = req.body.coordinates
-    // if(Array.isArray(coordinates))
-    if (!latitude || !longitude) res.send("Enter latitude between -90 and 90 and longitude between -180 and 180")
-    if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180) res.send("Enter latitude between -90 and 90 and longitude between -180 and 180")
+    if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180 || !latitude || !longitude) res.send("Enter latitude between -90 and 90 and longitude between -180 and 180")
     else {
         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=8bdbd54d3dcf72500df09e38fc2ed127`
         await request({ url: url, json: true }, (err, { body }) => {
-            if (body.name) city = body.name
-            if (city) {
-                res.send(`Today in the city ${city} weather ${body.weather[0].main.toLowerCase()} wind speed ${String(body.wind.speed)}m/h and temperature ${String(Math.floor(body.main.temp - 273))}℃`);
+            if (body.name) {
+                res.send(`Today in the city ${body.name} weather ${body.weather[0].main.toLowerCase()} wind speed ${String(body.wind.speed)}m/h and temperature ${String(Math.floor(body.main.temp - 273))}℃`);
             } else {
                 res.send(`Today at the coordinates ${latitude}, ${longitude} the weather ${body.weather[0].main.toLowerCase()} wind speed ${String(body.wind.speed)} m/h and temperature ${String(Math.floor(body.main.temp - 273))} ℃ `);
             }
