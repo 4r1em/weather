ad3bf3465180a4f75387cb69e6b011cd8e6247fe
4r1em <art.beloshapka@gmail.com>
2021-10-07 11:54:26 +0300

    revision

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const request = require("request");
 const app = new express();
-// const cites = ["kiev", "moscow", "london", "minsk"]
 
 app.use(express.json())
 app.post('/weather', async (req, res) => {
@@ -12,32 +11,29 @@ app.post('/weather', async (req, res) => {
     for (item of coordinates) {
         latitude = item[0]
         longitude = item[1]
-        if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180 || !latitude || !longitude) res.send("Enter latitude between -90 and 90 and longitude between -180 and 180")
+        if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180 || !latitude || !longitude) {
+            return res.send("Enter latitude between -90 and 90 and longitude between -180 and 180")
+        }
         else {
             const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=8bdbd54d3dcf72500df09e38fc2ed127`
             await request({ url: url, json: true }, (err, { body }) => {
+                const weather = body.weather[0].main.toLowerCase()
+                const city = body.name
+                const wind = String(body.wind.speed)
+                const temperature = String(Math.floor(body.main.temp - 273))
+
                 if (body.name) {
-                    arrEnd.push(`Today in the city ${body.name} weather ${body.weather[0].main.toLowerCase()} wind speed ${String(body.wind.speed)}m/h and temperature ${String(Math.floor(body.main.temp - 273))}℃`);
+                    arrEnd.push(`Today in the city ${city} weather ${weather}
+                    wind speed ${wind}m/h and temperature ${temperature}℃`);
                 } else {
-                    arrEnd.push(`Today at the coordinates ${latitude}, ${longitude} the weather ${body.weather[0].main.toLowerCase()} wind speed ${String(body.wind.speed)} m/h and temperature ${String(Math.floor(body.main.temp - 273))} ℃ `);
+                    arrEnd.push(`Today at the coordinates ${latitude}, ${longitude}
+                    the weather ${weather} wind speed ${wind} m/h and temperature ${temperature} ℃`);
                 }
-                if (coordinates.length == arrEnd.length) res.send(arrEnd.join("\n"))
+                if (coordinates.length == arrEnd.length) return res.send(arrEnd.join("\n"))
             })
         }
 
     }
-
-
-
-    // const city = req.body.city
-    // if (!city) res.send("Need city")
-    // if (!cites.includes(city.toLowerCase())) res.send(`Need city ${cites}`)
-    // else {
-    //     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8bdbd54d3dcf72500df09e38fc2ed127`
-    //     await request({ url: url, json: true }, (err, { body }) => {
-    //         res.send(`Today in the city ${city} weather ${body.weather[0].main.toLowerCase()} wind speed ${String(body.wind.speed)}km/h and temperature ${String(Math.floor(body.main.temp - 273))}℃`);
-    //     });
-    // }
 })
 app.listen(3020, () => {
     console.log("Server work");
